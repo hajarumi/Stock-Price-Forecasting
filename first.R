2dc4bc229326ac3b3b4fc1e9699f1dc5dc40fd94
hajarumi <hajar.umi.abdillah@gmail.com>
2022-05-30 10:09:52 +0700

    first commit

--- first.R
+++ first.R
@@ -4,15 +4,15 @@ hajarumits<-ts(hajarumi, frequency = 1, start = c(1,1))
 hajarumits
 #Prediksi(Forecasting)
 hajarumitsforecasts <- HoltWinters(hajarumits, beta=FALSE, gamma=FALSE)
-hajarumitsforecast
+hajarumitsforecasts
 hajarumitsforecasts$fitted
 hajarumitsforecasts$SSE
 HoltWinters(hajarumits, beta=FALSE, gamma=FALSE, l.start=0)
-#Prediksi(Forecasting Bulan ke 202-211)
+#Prediksi Bulan ke 20-29 (Forecast 20-29 months after)
 library(forecast)
 hajarumitsforecasts2<-"forecast"(hajarumitsforecasts, h=ifelse(frequency(hajarumitsforecasts$x)>1,2*frequency(object$x),10), level=c(80,95),fan=FALSE,lambda=NULL, biasadj=FALSE)
 hajarumitsforecasts2
-#Plot Prediksi(Forecasting Bulan ke 202-211)
+#Plot Prediksi (Prediction plot)
 plot(hajarumitsforecasts2)
 
 
